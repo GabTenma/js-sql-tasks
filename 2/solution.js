cb620bc7b606a5fd676bed51f8c720ed12a76702
GabTenma <hoseamattews24@mail.ru>
2024-11-08 18:50:11 +0300

    new remote origin

--- 2/solution.js
+++ 2/solution.js
@@ -8,5 +8,25 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+export default async function solution(articles) {
+  const sql = postgres(config);
 
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS articles (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255),
+      description VARCHAR(255)
+    )`;
+    
+    const ids = await sql`
+      INSERT INTO articles (title, description)
+      SELECT * FROM jsonb_to_recordset(${articles}) AS data(title TEXT, description TEXT)
+      RETURNING id
+    `;
+
+    return ids.map((obj) => obj.id);
+  } finally {
+    await sql.end();
+  }
+}
 // END
--- 4/solution.js
+++ 4/solution.js
@@ -8,5 +8,43 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+async function solution(user, roomNumber, price) {
+  const sql = postgres(config);
 
+  try {
+    await sql.begin(async (sql) => {
+      const userResult = await sql`
+        INSERT INTO users (username, phone)
+        VALUES (${user.username}, ${user.phone})
+        RETURNING id
+      `;
+
+      const userId = userResult[0].id;
+
+      const roomResult = await sql`
+        SELECT id FROM rooms
+        WHERE room_number = ${roomNumber}
+      `;
+
+      const roomId = roomResult[0].id;
+
+      await sql`
+        INSERT INTO orders (user_id, room_id, price)
+        VALUES (${userId}, ${roomId}, ${price})
+      `;
+
+      await sql`
+        UPDATE rooms
+        SET status = 'reserved'
+        WHERE id = ${roomId}
+      `;
+    });
+  } catch (error) {
+    throw new Error("Ошибка при бронировании");
+  } finally {
+    await sql.end();
+  }
+}
+
+export default solution;
 // END
